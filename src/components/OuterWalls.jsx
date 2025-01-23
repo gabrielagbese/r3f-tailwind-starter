8a05723c806758d3944ca97873523aba7ce5d1d7
Gabriel Agbese <gabriel.agbese2@gmail.com>
2025-01-23 10:56:18 +0000

    base starter r3f,jsx,plane with ecctrl movement, joystick

--- src/components/OuterWalls.jsx
+++ src/components/OuterWalls.jsx
@@ -1,19 +1,22 @@
 import { Box } from "@react-three/drei"
+import { RigidBody } from "@react-three/rapier"
 
 export default function OuterWalls() {
   const wallThickness = 0.5
-  const wallHeight = 5
+  const wallHeight = 3
   const planeSize = 40
 
   const WallSegment = ({ args, position }) => (
-    <Box args={args} position={position} castShadow receiveShadow>
-      <meshStandardMaterial
-        color="#4f4f4f"
-        roughness={0.7}
-        metalness={0.2}
-        envMapIntensity={0.5}
-      />
-    </Box>
+    <RigidBody type="fixed" colliders="trimesh">
+      <Box args={args} position={position} castShadow receiveShadow>
+        <meshStandardMaterial
+          color="#4f4f4f"
+          roughness={0.7}
+          metalness={0.2}
+          envMapIntensity={0.5}
+        />
+      </Box>
+    </RigidBody>
   )
 
   return (
